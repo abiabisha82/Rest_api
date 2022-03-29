7ded9a94924bd8aeae7bfcef2148e40b6578029e
abisha <abiabisha82@gmail.com>
2022-03-29 12:19:58 +0530

    urls commit

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import BaseUserManager
 
 
 # Create your models here.
+
+
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
@@ -17,6 +19,16 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name,)
 
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
